b684366bdf13f35a8420a14cea6b44cdab84d959
whatthefoobar <irina.mntn@gmail.com>
2022-07-13 09:57:47 +0200

    static products rendered w rating component

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -4,9 +4,9 @@ import Rating from './Rating';
 
 const Product = ({ product }) => {
   return (
-    <Card className="my-3 p-3 rounded">
+    <Card className="my-3 p-3 rounded card">
       <a href={`/product/${product._id}`}>
-        <Card.Img src={product.image} variant="top" />
+        <Card.Img src={product.image} variant="top" className="product-img" />
       </a>
 
       <Card.Body>
