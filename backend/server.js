f6178a5f4b0267688e86646a72441ee1cf027dd3
whatthefoobar <irina.mntn@gmail.com>
2022-07-20 18:55:53 +0200

    auth and jwt tonen generated

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ import colors from 'colors';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 import connectDB from './config/db.js';
 import productRoutes from './routes/productRoutes.js';
-// import userRoutes from './routes/userRoutes.js';
+import userRoutes from './routes/userRoutes.js';
 
 dotenv.config();
 
@@ -12,14 +12,14 @@ connectDB();
 
 const app = express();
 
-app.use(express.json());
+app.use(express.json()); // allows us to accept json data in the body
 
 app.get('/', (req, res) => {
   res.send('API is running...');
 });
 
 app.use('/api/products', productRoutes);
-// app.use('/api/users', userRoutes);
+app.use('/api/users', userRoutes);
 
 app.use(notFound);
 app.use(errorHandler);
