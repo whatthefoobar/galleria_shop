d92397671c6e92f1c05e90fdb2e53572bcdbc4f0
whatthefoobar <irina.mntn@gmail.com>
2022-07-21 10:46:22 +0200

    user register and pass hashing

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,7 +1,14 @@
 import express from 'express';
 const router = express.Router();
-import { authUser } from '../controllers/userController.js';
+import {
+  authUser,
+  registerUser,
+  getUserProfile,
+} from '../controllers/userController.js';
+import { protect } from '../middleware/authMiddleware.js';
 
+router.route('/').post(registerUser);
 router.post('/login', authUser);
+router.route('/profile').get(protect, getUserProfile); // add the protect middleware wherever we want to protect a route w jwt
 
 export default router;
