d58b3540bdb7937005e241cb2e6008e3fc816680
whatthefoobar <irina.mntn@gmail.com>
2022-07-22 09:23:23 +0200

    user login reducer action

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -6,19 +6,26 @@ import {
   productDetailsReducer,
 } from './reducers/productReducers';
 import { cartReducer } from './reducers/cartReducers';
+import { userLoginReducer } from './reducers/userReducers';
 
 const reducer = combineReducers({
   productList: productListReducer,
   productDetails: productDetailsReducer,
   cart: cartReducer,
+  userLogin: userLoginReducer,
 });
 
 const cartItemsFromStorage = localStorage.getItem('cartItems')
   ? JSON.parse(localStorage.getItem('cartItems'))
   : []; // we get our cart items previously saved in localStorage see cartActions.js
 
+const userInfoFromStorage = localStorage.getItem('userInfo')
+  ? JSON.parse(localStorage.getItem('userInfo'))
+  : null;
+
 const initialState = {
   cart: { cartItems: cartItemsFromStorage },
+  userLogin: { userInfo: userInfoFromStorage },
 }; // we put cart items previously saved in localStorage see cartActions.js in pur state
 
 const middleware = [thunk];
