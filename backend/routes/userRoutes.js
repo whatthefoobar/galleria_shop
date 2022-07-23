b405b5188ddf5cb4189de96ee0051989ae2af2c9
whatthefoobar <irina.mntn@gmail.com>
2022-07-23 10:49:33 +0200

    put route for update profile

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -4,11 +4,15 @@ import {
   authUser,
   registerUser,
   getUserProfile,
+  updateUserProfile,
 } from '../controllers/userController.js';
 import { protect } from '../middleware/authMiddleware.js';
 
 router.route('/').post(registerUser);
 router.post('/login', authUser);
-router.route('/profile').get(protect, getUserProfile); // add the protect middleware wherever we want to protect a route w jwt
+router
+  .route('/profile') // add the protect middleware wherever we want to protect a route w jwt
+  .get(protect, getUserProfile)
+  .put(protect, updateUserProfile);
 
 export default router;
