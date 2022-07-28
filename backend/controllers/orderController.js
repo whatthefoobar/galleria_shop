ab62b62f88cf770eb051c564ed5521f8708d54a9
whatthefoobar <irina.mntn@gmail.com>
2022-07-28 10:29:19 +0200

    Update order to paid endpoint (controller n route)

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -54,4 +54,30 @@ const getOrderById = asyncHandler(async (req, res) => {
   }
 });
 
-export { addOrderItems, getOrderById };
+// @desc    Update order to paid
+// @route   GET /api/orders/:id/pay
+//  Private
+const updateOrderToPaid = asyncHandler(async (req, res) => {
+  const order = await Order.findById(req.params.id);
+
+  if (order) {
+    order.isPaid = true;
+    order.paidAt = Date.now();
+    order.paymentResult = {
+      // this will come from Paypal
+      id: req.body.id, // from paypal response
+      status: req.body.status,
+      update_time: req.body.update_time,
+      email_address: req.body.payer.email_address,
+    };
+
+    const updatedOrder = await order.save();
+
+    res.json(updatedOrder);
+  } else {
+    res.status(404);
+    throw new Error('Order not found');
+  }
+});
+
+export { addOrderItems, getOrderById, updateOrderToPaid };
