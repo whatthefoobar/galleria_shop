f02cca27a610cd7f092fd057d2989fd901517a8d
whatthefoobar <irina.mntn@gmail.com>
2022-09-12 10:03:32 +0200

    make it better

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import store from './store';
 import './bootstrap.min.css';
 import './index.css';
 import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.render(
   <Provider store={store}>
     <App />
-  </Provider>
+  </Provider>,
+  document.getElementById('root')
 );
