5248d6af1226e7c25e75ca68fb4ac372d2d5128c
whatthefoobar <irina.mntn@gmail.com>
2022-09-19 15:22:00 +0200

    prep for deployment w heroku

--- backend/server.js
+++ backend/server.js
@@ -36,8 +36,21 @@ app.get('/api/config/paypal', (req, res) =>
   res.send(process.env.PAYPAL_CLIENT_ID)
 );
 
+// for deployment w heroku create a build to serve on heroku
 const __dirname = path.resolve();
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/frontend/build')));
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  );
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running....');
+  });
+}
 
 app.use(notFound);
 app.use(errorHandler);
