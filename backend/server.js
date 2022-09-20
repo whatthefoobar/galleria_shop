aa59474be5347a5046fe243de414b139827c7b53
whatthefoobar <irina.mntn@gmail.com>
2022-09-20 10:48:37 +0200

    prep for deployment w heroku

--- backend/server.js
+++ backend/server.js
@@ -16,10 +16,10 @@ connectDB();
 
 const app = express();
 
-// if (process.env.NODE_ENV === 'development') {
-//   // mw for morgan that runs only in production
-//   app.use(morgan('dev'));
-// }
+if (process.env.NODE_ENV === 'development') {
+  // mw for morgan that runs only in production
+  app.use(morgan('dev'));
+}
 
 app.use(express.json()); // allows us to accept json data in the body
 
@@ -44,7 +44,7 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '/frontend/build')));
 
   app.get('*', (req, res) =>
-    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+    res.sendFile(path.join(__dirname, '/frontend/build/index.html'))
   );
 } else {
   app.get('/', (req, res) => {
